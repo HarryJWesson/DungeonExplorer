3e5f44f1501970e974527cab84b5e9776f3bc90e
HarryJWesson <harry7wesson@gmail.com>
2025-03-05 21:22:21 +0000

    Implemented code to initialise a room and player Implemented code to present options to the player

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Media;
+using System.Xml.Serialization;
 
 namespace DungeonExplorer
 {
@@ -11,7 +12,12 @@ namespace DungeonExplorer
 
         public Game()
         {
-            // Initialize the game with one room and one player
+            Room room = new Room("The first room");
+            
+            Console.WriteLine("Please input the player's name: ");
+            string name = Console.ReadLine();
+
+            Player player = new Player(name, 100);
 
         }
         public void Start()
@@ -22,6 +28,19 @@ namespace DungeonExplorer
             {
                 // Code your playing logic here
                 // Present options the player can take > view the room, display health and inventory
+                try
+                {
+                    Console.WriteLine("Please input the relative number for your choice:  ");
+                    Console.WriteLine("1: View the room");
+                    Console.WriteLine("2: Check player stats");
+                    Console.WriteLine("3: Pick up the item in the room (If there is one)");
+                    Console.WriteLine("---------------------------");
+                    int choice = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Please input a valid choice!");
+                }
             }
         }
     }
